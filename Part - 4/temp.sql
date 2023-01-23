41bb3136fd305e1dc7c1229b0544c9c68d526067
Divyank Kulshrestha <52440757+devDivyank@users.noreply.github.com>
2023-01-23 12:58:10 -0500

    Delete Part - 4 directory

--- Part - 4/temp.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM member WHERE name LIKE 'Sigourney Weaver';
-
-SELECT * FROM actor_title_character as atc WHERE atc.actor = 244;
-SELECT * FROM actor_title_character as atc WHERE atc.actor = 10721564;
-SELECT * FROM actor_title_character as atc WHERE atc.actor = 13219017;
